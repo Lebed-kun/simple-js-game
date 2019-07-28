347937d7e3a47f4468887797f8c7abf170a3cfa4
Lebed-kun <lebed-kun@mail.ru>
2019-07-28 16:32:43 +0400

    Created models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    player_name = models.CharField(max_length=100)
+    score = models.IntegerField(default=0)
+    ip = models.GenericIPAddressField()
+    is_playing = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.player_name + ' ' + str(self.score)
+
+class GameInfo(models.Model):
+    info = models.TextField()
+
+    def __str__(self):
+        return self.info[:15]
+
+class Question(models.Model):
+    question = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.question
+
+class Answer(models.Model):
+    answer = models.CharField(max_length=50)
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='Answer')
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return str(self.question.id) + ' : ' + self.answer
+
--- server/settings.py
+++ server/settings.py
@@ -37,11 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+
+    'game',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,7 +59,7 @@ ROOT_URLCONF = 'server.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +123,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+        'rest_framework.permissions.AllowAny'
+    ]
+}
